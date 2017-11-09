c24af30636ff2f658df9f85300ae933ddfafb658
emilio <emi.alv@outlook.com>
2017-11-09 13:59:12 -0700

    More discussions added. Some hardcoded XAML removed.

--- shuttr/shuttr/DiscussionPage.xaml.cs
+++ shuttr/shuttr/DiscussionPage.xaml.cs
@@ -31,22 +31,33 @@ namespace shuttr
         {
             Discussion clickableDiscussion= new Discussion("User1", "Let's see your best nightlife photos!", 3);
             clickableDiscussion.MouseLeftButtonDown += new MouseButtonEventHandler(this.DiscussionClickTest);
+            clickableDiscussion.Name = "discussion1";
 
             discussionFeed.Children.Add(clickableDiscussion);
 
             discussionFeed.Children.Add(new Discussion("User2", "Looking to shoot film, what do I need to know?", 24));
+            discussionFeed.Children.Add(new Discussion("User3", "You all take beautiful photographs. Can I do the same with just an iPhone?", 23));
+            discussionFeed.Children.Add(new Discussion("User4", "Do you understand your histograms?", 9));
+            discussionFeed.Children.Add(new Discussion("User5", "Anyone excited for the new capture clip v3?", 0));
+            discussionFeed.Children.Add(new Discussion("User6", "Who else is excited for snow photography???", 29));
+            discussionFeed.Children.Add(new Discussion("User7", "Advice needed on having one of my pictures enlarged to 9ft tall.", 42));
         }
 
         private void DiscussionClick(object sender, MouseButtonEventArgs e)
         {
-            if (sender.Equals(discussion1))
+            if (sender.GetType().Equals(typeof(Discussion)))
             {
-                DiscussionPage popUp = new DiscussionPage();
-                popUp.popUpPageFill.Fill = new SolidColorBrush(Colors.Black);
-                popUp.popUpPageFill.Visibility = Visibility.Visible;
-                discussionContentControl.Content = popUp;
-                popUp.discussionPopUpWindow.IsOpen = true;
+                Discussion senderCast = (Discussion) sender;
+                if (senderCast.Name == "discussion1")
+                {
+                    DiscussionPage popUp = new DiscussionPage();
+                    popUp.popUpPageFill.Fill = new SolidColorBrush(Colors.Black);
+                    popUp.popUpPageFill.Visibility = Visibility.Visible;
+                    discussionContentControl.Content = popUp;
+                    popUp.discussionPopUpWindow.IsOpen = true;
+                }
             }
+            
         }
 
         private void DiscussionClickTest(object sender, MouseButtonEventArgs e)
