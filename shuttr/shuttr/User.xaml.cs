93836bc0eb87c10658054934c901274e7470d9bc
emilio <emi.alv@outlook.com>
2017-11-24 16:29:33 -0700

    Created a User object, storing a user's information

---
+++ shuttr/shuttr/User.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace shuttr
+{
+    /// <summary>
+    /// Interaction logic for User.xaml
+    /// </summary>
+    public partial class User : UserControl
+    {
+        /// <summary>
+        /// The user name of this user.
+        /// </summary>
+        private string userName;
+        public string UserName
+        {
+            get { return userName; }
+            set { userName = value; }
+        }
+
+        /// <summary>
+        /// The description a user chooses for their profile and/or themselves.
+        /// </summary>
+        private string biography;
+        public string Biography
+        {
+            get { return biography; }
+            set { biography = value; }
+        }
+
+        /// <summary>
+        /// The date the user joined, represented as a DateTime object.
+        /// Use DateTime.Today to get the current date.
+        /// </summary>
+        private DateTime dateJoined;
+        public DateTime DateJoined
+        {
+            get { return dateJoined; }
+            set { dateJoined = DateJoined; }
+        }
+
+        // Posts. list of UserControls
+
+        public User()
+        {
+            InitializeComponent();
+        }
+
+        public void setProfilePicture(BitmapImage image)
+        {
+            userPicture.Source = image;
+        }
+
+        public void setProfilePicture(string imagePath)
+        {
+            Uri imageUri = new Uri(imagePath, UriKind.Relative);
+            BitmapImage imageBitmap = new BitmapImage(imageUri);
+            userPicture.Source = imageBitmap;
+        }
+    }
+}
