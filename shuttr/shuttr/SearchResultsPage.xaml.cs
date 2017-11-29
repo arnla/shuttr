41c19d6b87a8e2c2db5ce409680fdff41d816a8a
emilio <emi.alv@outlook.com>
2017-11-28 18:05:52 -0700

    Creating a search results page, and testing a new sort menu

---
+++ shuttr/shuttr/SearchResultsPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace shuttr
+{
+    /// <summary>
+    /// Interaction logic for SearchResultsPage.xaml
+    /// </summary>
+    public partial class SearchResultsPage : UserControl
+    {
+        public SearchResultsPage()
+        {
+            InitializeComponent();
+        }
+
+        public void SortClick(object sender, EventArgs e)
+        {
+            if (sender.Equals(sortByMenu))
+            {
+                sortByDropdown.IsOpen = !sortByDropdown.IsOpen;
+            }
+            else if (sender.Equals(sortPopular))
+            {
+                currentSortOption.Content = "Popular";
+            }
+            else if (sender.Equals(sortNew))
+            {
+                currentSortOption.Content = "New";
+            }
+            else if (sender.Equals(sortMostCommented))
+            {
+                currentSortOption.Content = "Most Commented";
+            }
+            else if (sender.Equals(sortMostUpvoted))
+            {
+                currentSortOption.Content = "Most Upvoted";
+            }
+        }
+    }
+}
