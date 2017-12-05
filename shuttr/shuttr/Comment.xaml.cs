decb2a5bda6d49ba095a30a3261dbada7985f5d4
Lawrence Nguyen <lawrence.nguyen@ucalgary.ca>
2017-12-05 00:53:14 -0700

    Implemented not being able to reply to deleted comments

--- shuttr/shuttr/Comment.xaml.cs
+++ shuttr/shuttr/Comment.xaml.cs
@@ -22,6 +22,7 @@ namespace shuttr
     {
         private string username;
         private string comment;
+        private bool deleted;
         public UserControl parent { get; set; }
         private bool currentUser = false;
         public bool CurrentUser
@@ -57,6 +58,7 @@ namespace shuttr
             usernameText.Text = username;
             commentBox.Text = comment;
             CurrentUser = false;
+            deleted = false;
         }
 
         public Comment(string username, string comment, PhotoPopup parent)
@@ -68,6 +70,7 @@ namespace shuttr
             commentBox.Text = comment;
             this.parent = parent;
             CurrentUser = false;
+            deleted = false;
         }
 
         public Comment(string username, string comment, DiscussionPopup parent)
@@ -79,6 +82,7 @@ namespace shuttr
             commentBox.Text = comment;
             this.parent = parent;
             CurrentUser = false;
+            deleted = false;
         }
 
         protected void Button_Click(object sender, EventArgs e)
@@ -123,6 +127,8 @@ namespace shuttr
                 commentBox.Foreground = Brushes.Red;
                 usernameText.Foreground = Brushes.Red;
                 CurrentUser = false;
+                deleted = true;
+                replyButton.IsEnabled = false;
             }
         }
     }
