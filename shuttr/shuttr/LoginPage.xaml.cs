c0fd99315761cf4ff05dc75c3f728df1cbeb0cdd
emilio <emi.alv@outlook.com>
2017-12-05 15:00:27 -0700

    Login page now auto-focuses on username and logs in on enter

--- shuttr/shuttr/LoginPage.xaml.cs
+++ shuttr/shuttr/LoginPage.xaml.cs
@@ -27,6 +27,13 @@ namespace shuttr
             InitializeComponent();
             LoginSwitcher.loginSwitcher = this;
             this.main = main;
+
+            Loaded += LoginLoaded;
+        }
+
+        private void LoginLoaded(object sender, RoutedEventArgs e)
+        {
+            usernameBox.Focus();
         }
 
         public void Navigate(UserControl nextPage)
@@ -84,5 +91,41 @@ namespace shuttr
 
         public void Text_Changed(object sender, EventArgs e)
         { }
+
+        public void NextField(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                passBox.Focus();
+            }
+        }
+
+        public void CheckForEnter(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                bool formComplete = true;
+
+                if (passBox.Password == "")
+                {
+                    passwordBoxDefault.Foreground = Brushes.Red;
+                    passwordBoxDefault.Opacity = 1;
+                    formComplete = false;
+                    passBox.Focus();
+                }
+                if (usernameBox.Text == "")
+                {
+                    usernameBoxDefault.Foreground = Brushes.Red;
+                    usernameBoxDefault.Opacity = 1;
+                    formComplete = false;
+                    usernameBox.Focus();
+                }
+                if (formComplete)
+                {
+                    //SignupSwitcher.Switch(new FollowingPage());
+                    main.SignIn();
+                }
+            }
+        }
     }
 }
